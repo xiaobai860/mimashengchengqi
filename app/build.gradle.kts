7788dd4dd949a66322912963c4b38a727a440121
xx <byyx191@qq.com>
2026-08-10 21:57:33 +0800

    chore: 版本号升至 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lesspass.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         ndk {
             debugSymbolLevel = "none"
         }
